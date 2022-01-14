4f783d2e630e484b6e319f8a5deb8b150694bb1e
dohui ko <ksder99@likelion.org>
2022-01-14 19:43:58 +0900

    JS: Navbar

---
+++ main.js
@@ -0,0 +1,15 @@
+'user strict';
+
+
+// Mkae navbar transparent when it is on
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    console.log(`navbarHeight: ${navbarHeight}`);
+    if(window.scrollY > navbarHeight){
+        navbar.classList.add('navbar--dark');
+    }else {
+        navbar.classList.remove('navbar--dark');
+    }
+})
